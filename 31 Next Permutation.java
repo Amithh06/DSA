f5e2fc8623122135fb99a5e54da0d348a2e2e4c0
Amith H <amithh1366@gmail.com>
2026-04-17 18:48:06 +0530

    Next Permutation solution in java

---
+++ 31 Next Permutation.java
@@ -0,0 +1,45 @@
+/*A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+
+Example 1:
+Input: nums = [1,2,3]
+Output: [1,3,2] */
+// The Time and Space complexity of this solution :- Time complexity=O(n), Space complexity=O(1)
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        
+        int i = nums.length-2;
+        
+        while(i>=0 && nums[i+1] <= nums[i]){
+            i--;
+        }
+        if(i >= 0){
+            int j = nums.length - 1;
+            while(nums[j] <= nums[i]){
+                j--;
+            }
+            swap(nums,i,j);
+        }
+        reverse(nums, i+1);
+        
+    }
+    
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    public void reverse(int[] nums, int i){
+        int j= nums.length-1;
+        
+        while(i<j){
+            swap(nums, j, i);
+            i++;
+            j--;
+        }
+    }
+
+    
+}
