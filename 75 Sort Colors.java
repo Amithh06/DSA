6fe90b7a01f3b5fe31d79811c070c8a28b2031bd
Amith H <amithh1366@gmail.com>
2026-04-21 07:14:07 +0530

    75 Sort Colors solution in java

---
+++ 75 Sort Colors.java
@@ -0,0 +1,31 @@
+/*
+Example 1:
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+ */
+//The Time and Space complexity of this solution :- Time complexity=O(n), Space complexity=O(n)
+class Solution {
+    public void sortColors(int[] nums) {
+        int low=0;
+        int high=nums.length-1;
+        int current=0;
+        while(current<=high){
+            if(nums[current]==0){
+                swap(nums,current,low);
+                low++;
+                current++;
+            }else if(nums[current]==2){
+              swap(nums,current,high);
+                high--;
+                
+            }else{
+                current++;
+            }
+        }
+    }
+    public void swap(int[] nums,int i,int j) {
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+}
